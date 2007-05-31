f451aec9eae7f9cfe437d6d45053301d8a3c5e40
lth@acm.org <lth@acm.org>
2007-05-31 10:53:29 -0700

    Renamed Name to FullName to avoid a name clash
    --HG--
    branch : com.mozilla.es4.smlnj
    extra : convert_revision : 88d749d63cfdf8d53d1bb239c362442668066d84

--- tests/spidermonkey/ecma_3/RegExp/regress-103087.js
+++ tests/spidermonkey/ecma_3/RegExp/regress-103087.js
@@ -59,7 +59,7 @@ var rc = '';
 // the regexps are built in pieces  - 
 var NameStrt = "[A-Za-z_:]|[^\\x00-\\x7F]";
 var NameChar = "[A-Za-z0-9_:.-]|[^\\x00-\\x7F]";
-var Name = "(" + NameStrt + ")(" + NameChar + ")*";
+var FullName= "(" + NameStrt + ")(" + NameChar + ")*";
 var TextSE = "[^<]+";
 var UntilHyphen = "[^-]*-";
 var Until2Hyphens = UntilHyphen + "([^-]" + UntilHyphen + ")*-";
@@ -68,42 +68,42 @@ var UntilRSBs = "[^]]*]([^]]+])*]+";
 var CDATA_CE = UntilRSBs + "([^]>]" + UntilRSBs + ")*>";
 var S = "[ \\n\\t\\r]+";
 var QuoteSE = '"[^"]' + "*" + '"' + "|'[^']*'";
-var DT_IdentSE = S + Name + "(" + S + "(" + Name + "|" + QuoteSE + "))*";
+var DT_IdentSE = S + FullName+ "(" + S + "(" + FullName+ "|" + QuoteSE + "))*";
 var MarkupDeclCE = "([^]\"'><]+|" + QuoteSE + ")*>";
 var S1 = "[\\n\\r\\t ]";
 var UntilQMs = "[^?]*\\?+";
 var PI_Tail = "\\?>|" + S1 + UntilQMs + "([^>?]" + UntilQMs + ")*>";
-var DT_ItemSE = "<(!(--" + Until2Hyphens + ">|[^-]" + MarkupDeclCE + ")|\\?" + Name + "(" + PI_Tail + "))|%" + Name + ";|" + S;
+var DT_ItemSE = "<(!(--" + Until2Hyphens + ">|[^-]" + MarkupDeclCE + ")|\\?" + FullName+ "(" + PI_Tail + "))|%" + FullName+ ";|" + S;
 var DocTypeCE = DT_IdentSE + "(" + S + ")?(\\[(" + DT_ItemSE + ")*](" + S + ")?)?>?";
 var DeclCE = "--(" + CommentCE + ")?|\\[CDATA\\[(" + CDATA_CE + ")?|DOCTYPE(" + DocTypeCE + ")?";
-var PI_CE = Name + "(" + PI_Tail + ")?";
-var EndTagCE = Name + "(" + S + ")?>?";
+var PI_CE = FullName+ "(" + PI_Tail + ")?";
+var EndTagCE = FullName+ "(" + S + ")?>?";
 var AttValSE = '"[^<"]' + "*" + '"' + "|'[^<']*'";
-var ElemTagCE = Name + "(" + S + Name + "(" + S + ")?=(" + S + ")?(" + AttValSE + "))*(" + S + ")?/?>?";
+var ElemTagCE = FullName+ "(" + S + FullName+ "(" + S + ")?=(" + S + ")?(" + AttValSE + "))*(" + S + ")?/?>?";
 var MarkupSPE = "<(!(" + DeclCE + ")?|\\?(" + PI_CE + ")?|/(" + EndTagCE + ")?|(" + ElemTagCE + ")?)";
 var XML_SPE = TextSE + "|" + MarkupSPE;
 var CommentRE = "<!--" + Until2Hyphens + ">";
 var CommentSPE = "<!--(" + CommentCE + ")?";
-var PI_RE = "<\\?" + Name + "(" + PI_Tail + ")";
+var PI_RE = "<\\?" + FullName+ "(" + PI_Tail + ")";
 var Erroneous_PI_SE = "<\\?[^?]*(\\?[^>]+)*\\?>";
 var PI_SPE = "<\\?(" + PI_CE + ")?";
 var CDATA_RE = "<!\\[CDATA\\[" + CDATA_CE;
 var CDATA_SPE = "<!\\[CDATA\\[(" + CDATA_CE + ")?";
 var ElemTagSE = "<(" + NameStrt + ")([^<>\"']+|" + AttValSE + ")*>";
-var ElemTagRE = "<" + Name + "(" + S + Name + "(" + S + ")?=(" + S + ")?(" + AttValSE + "))*(" + S + ")?/?>";
+var ElemTagRE = "<" + FullName+ "(" + S + FullName+ "(" + S + ")?=(" + S + ")?(" + AttValSE + "))*(" + S + ")?/?>";
 var ElemTagSPE = "<" + ElemTagCE;
-var EndTagRE = "</" + Name + "(" + S + ")?>";
+var EndTagRE = "</" + FullName+ "(" + S + ")?>";
 var EndTagSPE = "</(" + EndTagCE + ")?";
 var DocTypeSPE = "<!DOCTYPE(" + DocTypeCE + ")?";
-var PERef_APE = "%(" + Name + ";?)?";
+var PERef_APE = "%(" + FullName+ ";?)?";
 var HexPart = "x([0-9a-fA-F]+;?)?";
 var NumPart = "#([0-9]+;?|" + HexPart + ")?";
-var CGRef_APE = "&(" + Name + ";?|" + NumPart + ")?";
+var CGRef_APE = "&(" + FullName+ ";?|" + NumPart + ")?";
 var Text_PE = CGRef_APE + "|[^&]+";
 var EntityValue_PE = CGRef_APE + "|" + PERef_APE + "|[^%&]+";
 
 
-var rePatterns = new Array(AttValSE, CDATA_CE, CDATA_RE, CDATA_SPE, CGRef_APE, CommentCE, CommentRE, CommentSPE, DT_IdentSE, DT_ItemSE, DeclCE, DocTypeCE, DocTypeSPE, ElemTagCE, ElemTagRE, ElemTagSE, ElemTagSPE, EndTagCE, EndTagRE, EndTagSPE, EntityValue_PE, Erroneous_PI_SE, HexPart, MarkupDeclCE, MarkupSPE, Name, NameChar, NameStrt, NumPart, PERef_APE, PI_CE, PI_RE, PI_SPE, PI_Tail, QuoteSE, S, S1, TextSE, Text_PE, Until2Hyphens, UntilHyphen, UntilQMs, UntilRSBs, XML_SPE);
+var rePatterns = new Array(AttValSE, CDATA_CE, CDATA_RE, CDATA_SPE, CGRef_APE, CommentCE, CommentRE, CommentSPE, DT_IdentSE, DT_ItemSE, DeclCE, DocTypeCE, DocTypeSPE, ElemTagCE, ElemTagRE, ElemTagSE, ElemTagSPE, EndTagCE, EndTagRE, EndTagSPE, EntityValue_PE, Erroneous_PI_SE, HexPart, MarkupDeclCE, MarkupSPE, FullName, NameChar, NameStrt, NumPart, PERef_APE, PI_CE, PI_RE, PI_SPE, PI_Tail, QuoteSE, S, S1, TextSE, Text_PE, Until2Hyphens, UntilHyphen, UntilQMs, UntilRSBs, XML_SPE);
 
 
 // here's a big string to test the regexps on -
@@ -145,6 +145,7 @@ function test()
 
   for (var i=0; i<rePatterns.length; i++)
   {
+      print(rePatterns[i]);
     status = inSection(i);
     re = new RegExp(rePatterns[i]);
 
